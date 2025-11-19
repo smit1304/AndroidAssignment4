f2b927c59f45ff8fa160f4baccfaf90aa4fe4337
Sanjeev Chauhan <sanjeevchauhan3090@gmail.com>
2025-11-18 22:14:28 -0500

    Add secrets-gradle-plugin for API key management
    Integrated com.google.android.libraries.mapsplatform.secrets-gradle-plugin to improve API key hiding and management in the project.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    // For API Key Hiding
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1"
 }
 
 android {
